369d5e3bc9b8f1a2b450d6da0e5d2c165e852fe7
digimokan <digimokan@users.noreply.github.com>
2022-10-28 19:39:51 -0500

    Split up ansible and py-psutil installs in script

--- configure.sh
+++ configure.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 # FreeBSD ansible won't auto-update to latest pyXX-ansible version, so do this
-echo "Installing the latest 'pyXX-ansible' and supporting packages...."
-pkg install --yes sysutils/ansible sysutils/py-psutil
+echo "Installing the latest 'pyXX-ansible' package...."
+pkg install --yes sysutils/ansible
+
+# NOTE: py-psutil is required for dconf module for MATE
+#   - see https://docs.ansible.com/ansible/latest/collections/community/general/dconf_module.html#notes
+echo "Installing the latest 'py-psutil' package...."
+pkg install --yes sysutils/py-psutil
 
 # use ansible-galaxy cmd to download roles & collections from github/galaxy/etc
 ansible-galaxy install \
